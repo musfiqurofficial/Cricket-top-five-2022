71838d11776c461aebf6275142723d90962dfa77
musfiqurofficial <musfiqurofficial1999@gmail.com>
2022-08-23 17:49:32 +0600

    All most done

---
+++ js/main.js
@@ -0,0 +1,15 @@
+document.getElementById("calculate-btn").addEventListener("click", function () {
+    const playerInputField = document.getElementById("player-field").value;
+    const playerCost = playerInputField * 5;
+    const playerTotalCost = document.getElementById("player-cost").innerHTML = playerCost;
+
+    document.getElementById("total-calculate-btn").addEventListener("click", function () {
+        const managerFeeString = document.getElementById("manager-fee-field").value;
+        const managerFee = parseInt(managerFeeString);
+        const coachFeeString = document.getElementById("coach-fee-field").value;
+        const coachFee = parseInt(coachFeeString);
+        const totalCost = playerCost + managerFee + coachFee;
+        document.getElementById("total-cost").innerHTML = totalCost;
+    })
+
+})
---
+++ js/selectFive.js
@@ -0,0 +1,27 @@
+
+// function selectFive(element) {
+//     const selectFivePlayer = element.parentNode.children[0].innerText;
+
+//     const listParent = document.getElementById("olListPlayer");
+//     const li = document.createElement('li');
+//     li.innerText = selectFivePlayer;
+
+//     listParent.appendChild(li);
+// }
+
+const liArray = [];
+const allBtn = document.querySelectorAll('.my-btn-class');
+for (const btn of allBtn) {
+    btn.addEventListener('click', function (event) {
+        const parentUl = document.getElementById('olListPlayer');
+        const newLi = document.createElement('li');
+        newLi.innerText = event.target.parentNode.parentNode.children[1].children[0].innerText;
+        liArray.push(newLi);
+        if (liArray.length > 5) {
+            alert('You can not add more than 5 items elements!')
+        } else {
+            parentUl.appendChild(newLi);
+            btn.disabled = true;
+        }
+    })
+}
